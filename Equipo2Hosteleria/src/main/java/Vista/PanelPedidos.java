32a6323e7ca5f50aa8516d99d9b3e9e8a8543a00
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-01-22 12:49:44 +0100

    Retoques Panel Pedidos

--- Equipo2Hosteleria/src/main/java/Vista/PanelPedidos.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelPedidos.java
@@ -3,6 +3,7 @@ package Vista;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,10 +12,12 @@ import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
-public class PanelPedidos extends JPanel {
+public class PanelPedidos extends JPanel implements ChangeListener {
 
 	private JButton btnVolver;
 	private JButton btnDesconectarse;
@@ -23,8 +26,11 @@ public class PanelPedidos extends JPanel {
 	private JButton btnListaCompra;
 	private JComboBox comboBox_Productos;
 	private JComboBox comboBox_Cantidad;
+	private JLabel lblDireccin;
 	private JRadioButton rdbtnRecogerEnEstablecimiento;
 	private JRadioButton rdbtnEntregaADomicilio;
+	private ButtonGroup grupoBotones;
+	private JTextPane textPane_Direccion;
 	private ControladorPanelPedidos controladorPanelPedidos;
 	
 	public PanelPedidos(ControladorPanelPedidos controladorPanelPedidos)
@@ -36,53 +42,61 @@ public class PanelPedidos extends JPanel {
 		JLabel lblPedidos = new JLabel("Pedidos");
 		lblPedidos.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPedidos.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblPedidos.setBounds(58, 35, 328, 14);
+		lblPedidos.setBounds(38, 24, 362, 14);
 		add(lblPedidos);
 		
 		btnVolver = new JButton("Volver");
-		btnVolver.setBounds(58, 246, 89, 23);
+		btnVolver.setBounds(38, 235, 89, 23);
 		add(btnVolver);
 		
 		comboBox_Productos = new JComboBox();
-		comboBox_Productos.setBounds(58, 95, 173, 22);
+		comboBox_Productos.setBounds(38, 84, 173, 22);
 		add(comboBox_Productos);
 		
 		JLabel lblProductos = new JLabel("Productos:");
 		lblProductos.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblProductos.setBounds(58, 60, 115, 14);
+		lblProductos.setBounds(38, 49, 115, 14);
 		add(lblProductos);
 		
 		JLabel lblCantidad = new JLabel("Cantidad:");
 		lblCantidad.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblCantidad.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblCantidad.setBounds(271, 60, 115, 14);
+		lblCantidad.setBounds(285, 49, 115, 14);
 		add(lblCantidad);
 		
+		grupoBotones = new ButtonGroup();
+		
 		rdbtnRecogerEnEstablecimiento = new JRadioButton("Recoger en establecimiento");
-		rdbtnRecogerEnEstablecimiento.setBounds(58, 177, 173, 23);
+		rdbtnRecogerEnEstablecimiento.setBounds(38, 166, 197, 23);
+		rdbtnRecogerEnEstablecimiento.addChangeListener(this);
 		add(rdbtnRecogerEnEstablecimiento);
+		grupoBotones.add(rdbtnRecogerEnEstablecimiento);
 		
 		rdbtnEntregaADomicilio = new JRadioButton("Entrega a domicilio");
 		rdbtnEntregaADomicilio.setHorizontalAlignment(SwingConstants.RIGHT);
-		rdbtnEntregaADomicilio.setBounds(247, 177, 139, 23);
+		rdbtnEntregaADomicilio.setBounds(254, 166, 146, 23);
+		rdbtnEntregaADomicilio.addChangeListener(this);
 		add(rdbtnEntregaADomicilio);
+		grupoBotones.add(rdbtnEntregaADomicilio);
 		
-		JTextPane textPane_Direccion = new JTextPane();
-		textPane_Direccion.setBounds(213, 207, 173, 22);
+		textPane_Direccion = new JTextPane();
+		textPane_Direccion.setEnabled(false);
+		textPane_Direccion.setBounds(211, 196, 189, 22);
 		add(textPane_Direccion);
 		
 		comboBox_Cantidad = new JComboBox();
-		comboBox_Cantidad.setBounds(326, 95, 60, 22);
+		comboBox_Cantidad.setBounds(340, 84, 60, 22);
 		add(comboBox_Cantidad);
 		
 		btnAadirAlCarro = new JButton("A\u00F1adir al carro");
-		btnAadirAlCarro.setBounds(58, 147, 173, 23);
+		btnAadirAlCarro.setBounds(38, 136, 146, 23);
 		add(btnAadirAlCarro);
 		
-		JLabel lblDireccin = new JLabel("Direcci\u00F3n de entrega:");
+		lblDireccin = new JLabel("Direcci\u00F3n de entrega:");
+		lblDireccin.setEnabled(false);
 		lblDireccin.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblDireccin.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblDireccin.setBounds(58, 207, 139, 23);
+		lblDireccin.setBounds(56, 196, 139, 23);
 		add(lblDireccin);
 		
 		btnListaCompra = new JButton("Ver lista de la compra");
@@ -90,15 +104,16 @@ public class PanelPedidos extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnListaCompra.setBounds(247, 147, 139, 23);
+		btnListaCompra.setBounds(227, 136, 173, 23);
 		add(btnListaCompra);
 		
 		btnDesconectarse = new JButton("Desconectarse");
-		btnDesconectarse.setBounds(170, 246, 103, 23);
+		btnDesconectarse.setBounds(137, 235, 146, 23);
 		add(btnDesconectarse);
 		
 		btnPasarACaja = new JButton("Pasar a caja");
-		btnPasarACaja.setBounds(295, 246, 91, 23);
+		btnPasarACaja.setEnabled(false);
+		btnPasarACaja.setBounds(293, 235, 107, 23);
 		add(btnPasarACaja);
 		
 		initializeEvents();
@@ -110,6 +125,20 @@ public class PanelPedidos extends JPanel {
 		this.btnPasarACaja.addActionListener(listenerBotonPasarACaja(this.controladorPanelPedidos));
 		this.btnAadirAlCarro.addActionListener(listenerBotonAadirAlCarro(this.controladorPanelPedidos));
 		this.btnListaCompra.addActionListener(listenerBotonListaCompra(this.controladorPanelPedidos));
+	}
+	
+	public void stateChanged(ChangeEvent e){
+        
+		if (rdbtnRecogerEnEstablecimiento.isSelected()) {
+			lblDireccin.setEnabled(false);
+			textPane_Direccion.setEnabled(false);
+			btnPasarACaja.setEnabled(true);
+		}
+		if (rdbtnEntregaADomicilio.isSelected()) {
+			lblDireccin.setEnabled(true);
+			textPane_Direccion.setEnabled(true);
+			btnPasarACaja.setEnabled(false);
+		}
 	}
 	
 	private ActionListener listenerBotonVolver(ControladorPanelPedidos controladorPanelPedidos) {

909dd8d477c859908694b519c887cfefc844b7c1
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-01-22 09:38:47 +0100

    Panel de Pedidos WIP_1

--- Equipo2Hosteleria/src/main/java/Vista/PanelPedidos.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelPedidos.java
@@ -7,6 +7,11 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import Controlador.ControladorPanelGeneros;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 public class PanelPedidos extends JPanel {
@@ -21,13 +26,51 @@ public class PanelPedidos extends JPanel {
 		
 		setLayout(null);
 		
-		lblGeneros = new JLabel("Panel Generos");
-		lblGeneros.setBounds(58, 35, 115, 14);
+		lblGeneros = new JLabel("Pedidos");
+		lblGeneros.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGeneros.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblGeneros.setBounds(58, 35, 328, 14);
 		add(lblGeneros);
 		
 		btnVolver = new JButton("Volver");
-		btnVolver.setBounds(58, 62, 89, 23);
+		btnVolver.setBounds(58, 246, 89, 23);
 		add(btnVolver);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(58, 114, 173, 22);
+		add(comboBox);
+		
+		JLabel lblProductos = new JLabel("Productos:");
+		lblProductos.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblProductos.setBounds(58, 79, 115, 14);
+		add(lblProductos);
+		
+		JLabel lblCantidad = new JLabel("Cantidad:");
+		lblCantidad.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCantidad.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblCantidad.setBounds(271, 79, 115, 14);
+		add(lblCantidad);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Recoger en establecimiento");
+		rdbtnNewRadioButton.setBounds(58, 187, 173, 23);
+		add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnEntregaADomicilio = new JRadioButton("Entrega a domicilio");
+		rdbtnEntregaADomicilio.setHorizontalAlignment(SwingConstants.RIGHT);
+		rdbtnEntregaADomicilio.setBounds(213, 187, 173, 23);
+		add(rdbtnEntregaADomicilio);
+		
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setBounds(247, 217, 139, 22);
+		add(textPane_1);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(326, 114, 60, 22);
+		add(comboBox_1);
+		
+		JButton btnAadirAlCarro = new JButton("A\u00F1adir al carro");
+		btnAadirAlCarro.setBounds(58, 157, 173, 23);
+		add(btnAadirAlCarro);
 		
 		initializeEvents();
 	}

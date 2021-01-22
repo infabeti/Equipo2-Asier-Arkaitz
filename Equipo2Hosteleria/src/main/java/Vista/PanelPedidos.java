d65a873fb4fe980e0777891b74b54d24bdea125a
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-01-22 10:00:12 +0100

    Panel de Pedidos

--- Equipo2Hosteleria/src/main/java/Vista/PanelPedidos.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelPedidos.java
@@ -37,40 +37,66 @@ public class PanelPedidos extends JPanel {
 		add(btnVolver);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(58, 114, 173, 22);
+		comboBox.setBounds(58, 95, 173, 22);
 		add(comboBox);
 		
 		JLabel lblProductos = new JLabel("Productos:");
 		lblProductos.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblProductos.setBounds(58, 79, 115, 14);
+		lblProductos.setBounds(58, 60, 115, 14);
 		add(lblProductos);
 		
 		JLabel lblCantidad = new JLabel("Cantidad:");
 		lblCantidad.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblCantidad.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblCantidad.setBounds(271, 79, 115, 14);
+		lblCantidad.setBounds(271, 60, 115, 14);
 		add(lblCantidad);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Recoger en establecimiento");
-		rdbtnNewRadioButton.setBounds(58, 187, 173, 23);
+		rdbtnNewRadioButton.setBounds(58, 177, 173, 23);
 		add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnEntregaADomicilio = new JRadioButton("Entrega a domicilio");
 		rdbtnEntregaADomicilio.setHorizontalAlignment(SwingConstants.RIGHT);
-		rdbtnEntregaADomicilio.setBounds(213, 187, 173, 23);
+		rdbtnEntregaADomicilio.setBounds(247, 177, 139, 23);
 		add(rdbtnEntregaADomicilio);
 		
 		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(247, 217, 139, 22);
+		textPane_1.setBounds(213, 207, 173, 22);
 		add(textPane_1);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(326, 114, 60, 22);
+		comboBox_1.setBounds(326, 95, 60, 22);
 		add(comboBox_1);
 		
 		JButton btnAadirAlCarro = new JButton("A\u00F1adir al carro");
-		btnAadirAlCarro.setBounds(58, 157, 173, 23);
+		btnAadirAlCarro.setBounds(58, 147, 173, 23);
 		add(btnAadirAlCarro);
+		
+		JLabel lblDireccin = new JLabel("Direcci\u00F3n de entrega:");
+		lblDireccin.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblDireccin.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDireccin.setBounds(58, 207, 139, 23);
+		add(lblDireccin);
+		
+		JButton btnVerListaDe = new JButton("Ver lista de la compra");
+		btnVerListaDe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnVerListaDe.setBounds(247, 147, 139, 23);
+		add(btnVerListaDe);
+		
+		JButton btnDesconectarse = new JButton("Desconectarse");
+		btnDesconectarse.setBounds(170, 246, 103, 23);
+		add(btnDesconectarse);
+		
+		JButton btnPasarACaja = new JButton("Pasar a caja");
+		btnPasarACaja.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnPasarACaja.setBounds(295, 246, 91, 23);
+		add(btnPasarACaja);
 		
 		initializeEvents();
 	}

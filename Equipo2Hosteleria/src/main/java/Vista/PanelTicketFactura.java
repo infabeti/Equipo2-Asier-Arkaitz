5a612db159de375b92f8e28554b674b3881fdf77
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-01-25 08:52:12 +0100

    Ajuste de la clase PanelTicketFactura

--- Equipo2Hosteleria/src/main/java/Vista/PanelTicketFactura.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelTicketFactura.java
@@ -3,10 +3,13 @@ package Vista;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
 
 import Controlador.ControladorPanelBienvenida;
 import Controlador.ControladorPanelComandas;
@@ -25,13 +28,35 @@ public class PanelTicketFactura extends JPanel{
 
 	private JButton btnVolver;
 	private JButton btnDesconectarse;
+	private JRadioButton rdbtnTicket;
+	private JRadioButton rdbtnFactura;
+	private JLabel lblTicketFactura;
+	private JLabel lblTransaccion;
+	private JLabel lblFecha;
+	private JLabel lblLocal;
+	private JLabel lblProductos;
+	private JLabel lblNif;
+	private JLabel lblNombre;
+	private JLabel lblApellidos;
+	private JLabel lblTotal;
+	private JTextPane textPane_Fecha;
+	private JTextPane textPane_NºTransaccion;
+	private JTextPane textPane_Local;
+	private JTextPane textPane_NIF;
+	private JTextPane textPane_Nombre;
+	private JTextPane textPane_Apellidos;
+	private JTextPane textPane_Total;
+	private JTextPane textPane_Productos;
+	private JScrollPane scrollPane;
+	private ButtonGroup grupoBotones;
+	
 	private ControladorPanelTicketFactura controladorPanelTicketFactura;
 	
 	public PanelTicketFactura(ControladorPanelTicketFactura controladorPanelTicketFactura) {
 		this.controladorPanelTicketFactura = controladorPanelTicketFactura;
 		setLayout(null);
 		
-		JLabel lblTicketFactura = new JLabel("Ticket o Factura");
+		lblTicketFactura = new JLabel("Ticket o Factura");
 		lblTicketFactura.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTicketFactura.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblTicketFactura.setBounds(38, 11, 362, 14);
@@ -45,81 +70,85 @@ public class PanelTicketFactura extends JPanel{
 		btnVolver.setBounds(38, 235, 89, 23);
 		add(btnVolver);
 		
-		JLabel lblTransaccion = new JLabel("N\u00BATransaccion");
+		lblTransaccion = new JLabel("N\u00BATransaccion");
 		lblTransaccion.setBounds(10, 42, 89, 14);
 		add(lblTransaccion);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(49, 76, 129, 20);
-		add(textPane);
+		textPane_Fecha = new JTextPane();
+		textPane_Fecha.setBounds(49, 76, 129, 20);
+		add(textPane_Fecha);
 		
-		JLabel lblFecha = new JLabel("Fecha");
+		lblFecha = new JLabel("Fecha");
 		lblFecha.setBounds(10, 76, 89, 14);
 		add(lblFecha);
 		
-		JLabel lblLocal = new JLabel("Local");
+		lblLocal = new JLabel("Local");
 		lblLocal.setBounds(10, 110, 89, 14);
 		add(lblLocal);
 		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(101, 36, 129, 20);
-		add(textPane_1);
+		textPane_NºTransaccion = new JTextPane();
+		textPane_NºTransaccion.setBounds(101, 36, 129, 20);
+		add(textPane_NºTransaccion);
 		
-		JTextPane textPane_2 = new JTextPane();
-		textPane_2.setBounds(49, 107, 129, 20);
-		add(textPane_2);
+		textPane_Local = new JTextPane();
+		textPane_Local.setBounds(49, 107, 129, 20);
+		add(textPane_Local);
 		
-		JLabel lblProductos = new JLabel("Productos");
+		lblProductos = new JLabel("Productos");
 		lblProductos.setBounds(10, 152, 89, 14);
 		add(lblProductos);
 		
-		JRadioButton rdbtnTicket = new JRadioButton("Ticket");
+		rdbtnTicket = new JRadioButton("Ticket");
 		rdbtnTicket.setBounds(246, 38, 71, 23);
 		add(rdbtnTicket);
+		grupoBotones.add(rdbtnTicket);
 		
-		JRadioButton rdbtnFactura = new JRadioButton("Factura");
+		rdbtnFactura = new JRadioButton("Factura");
 		rdbtnFactura.setHorizontalAlignment(SwingConstants.LEFT);
 		rdbtnFactura.setBounds(319, 38, 81, 23);
 		add(rdbtnFactura);
+		grupoBotones.add(rdbtnFactura);
 		
-		JLabel lblNif = new JLabel("NIF");
+		lblNif = new JLabel("NIF");
 		lblNif.setBounds(202, 76, 89, 14);
 		add(lblNif);
 		
-		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre = new JLabel("Nombre");
 		lblNombre.setBounds(202, 101, 63, 14);
 		add(lblNombre);
 		
-		JLabel lblApellidos = new JLabel("Apellidos");
+		lblApellidos = new JLabel("Apellidos");
 		lblApellidos.setBounds(202, 126, 89, 14);
 		add(lblApellidos);
 		
-		JTextPane textPane_4 = new JTextPane();
-		textPane_4.setBounds(227, 76, 129, 20);
-		add(textPane_4);
+		textPane_NIF = new JTextPane();
+		textPane_NIF.setBounds(227, 76, 129, 20);
+		add(textPane_NIF);
 		
-		JTextPane textPane_5 = new JTextPane();
-		textPane_5.setBounds(256, 101, 129, 20);
-		add(textPane_5);
+		textPane_Nombre = new JTextPane();
+		textPane_Nombre.setBounds(256, 101, 129, 20);
+		add(textPane_Nombre);
 		
-		JTextPane textPane_6 = new JTextPane();
-		textPane_6.setBounds(266, 126, 129, 20);
-		add(textPane_6);
+		textPane_Apellidos = new JTextPane();
+		textPane_Apellidos.setBounds(266, 126, 129, 20);
+		add(textPane_Apellidos);
 		
-		JLabel lblTotal = new JLabel("Total");
+		lblTotal = new JLabel("Total");
 		lblTotal.setBounds(311, 239, 45, 14);
 		add(lblTotal);
 		
-		JTextPane textPane_6_1 = new JTextPane();
-		textPane_6_1.setBounds(343, 238, 63, 20);
-		add(textPane_6_1);
+		textPane_Total = new JTextPane();
+		textPane_Total.setBounds(343, 238, 63, 20);
+		add(textPane_Total);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 177, 408, 47);
 		add(scrollPane);
 		
-		JTextPane textPane_2_1 = new JTextPane();
-		scrollPane.setViewportView(textPane_2_1);
+		textPane_Productos = new JTextPane();
+		scrollPane.setViewportView(textPane_Productos);
+		
+		grupoBotones = new ButtonGroup();
 		
 		initializeEvents();
 	}
@@ -127,6 +156,26 @@ public class PanelTicketFactura extends JPanel{
 	private void initializeEvents() {
 		this.btnVolver.addActionListener(listenerBotonVolver(this.controladorPanelTicketFactura));
 		this.btnDesconectarse.addActionListener(listenerBotonDesconectarse(this.controladorPanelTicketFactura));
+	}
+	
+	public void stateChanged(ChangeEvent e){
+        
+		if (rdbtnTicket.isSelected()) {
+			lblNif.setEnabled(false);
+			lblNombre.setEnabled(false);
+			lblApellidos.setEnabled(false);
+			textPane_NIF.setEnabled(false);
+			textPane_Nombre.setEnabled(false);
+			textPane_Apellidos.setEnabled(false);
+		}
+		if (rdbtnFactura.isSelected()) {
+			lblNif.setEnabled(true);
+			lblNombre.setEnabled(true);
+			lblApellidos.setEnabled(true);
+			textPane_NIF.setEnabled(true);
+			textPane_Nombre.setEnabled(true);
+			textPane_Apellidos.setEnabled(true);
+		}
 	}
 	
 	private ActionListener listenerBotonVolver(ControladorPanelTicketFactura controladorPanelTicketFactura) {

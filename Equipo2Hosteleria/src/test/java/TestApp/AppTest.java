68185839380bc02013059e7abd2e40c892935360
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-01-25 10:59:58 +0100

    Archivo TestApp Creado

---
+++ Equipo2Hosteleria/src/test/java/TestApp/AppTest.java
@@ -0,0 +1,17 @@
+package TestApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Controlador.Controlador;
+import POO.Producto;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

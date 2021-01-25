9d7a47775c97eb4da6ff9913e860f6a7406db8c3
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-01-25 09:17:15 +0100

    Clase Producto creada y su package, productos creados en main

--- Equipo2Hosteleria/src/main/java/App/Main.java
+++ Equipo2Hosteleria/src/main/java/App/Main.java
@@ -2,6 +2,7 @@ package App;
 import Controlador.Controlador;
 import Modelo.*;
 import Modelo.Modelo;
+import POO.Producto;
 import Vista.Vista;
 
 public class Main {
@@ -18,4 +19,10 @@ public class Main {
 		controlador = new Controlador(modelo, vista);
 	}
 
+	Producto Producto1 = new Producto("Garbanzos","Comida","30-01-2021",5,5);	
+	Producto Producto2 = new Producto("Paella","Comida","31-01-2021",5,5);	
+	Producto Producto3 = new Producto("Bacalao","Comida","02-02-2021",5,5);	
+	Producto Producto4 = new Producto("Hamburguesa","Comida","30-01-2021",5,5);	
+	Producto Producto5 = new Producto("Cerveza","Bebida","01-02-2021",5,5);	
+	
 }

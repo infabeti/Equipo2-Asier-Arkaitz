b54c490b17819b2558cb45e8ab96024806db7793
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-01-26 10:50:09 +0100

    Limpieza de Main

--- Equipo2Hosteleria/src/main/java/App/Main.java
+++ Equipo2Hosteleria/src/main/java/App/Main.java
@@ -18,11 +18,5 @@ public class Main {
                 
 		controlador = new Controlador(modelo, vista);
 	}
-
-	Producto Producto1 = new Producto("Garbanzos","Comida","30-01-2021",5,5);	
-	Producto Producto2 = new Producto("Paella","Comida","31-01-2021",5,5);	
-	Producto Producto3 = new Producto("Bacalao","Comida","02-02-2021",5,5);	
-	Producto Producto4 = new Producto("Hamburguesa","Comida","30-01-2021",5,5);	
-	Producto Producto5 = new Producto("Cerveza","Bebida","01-02-2021",5,5);	
 	
 }

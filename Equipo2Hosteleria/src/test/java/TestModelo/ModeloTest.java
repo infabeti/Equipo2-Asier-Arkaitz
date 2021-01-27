1116c3b70dc6a02037e5db861601ccf3e5c14972
Asierhernandezga <75242363+Asierhernandezga@users.noreply.github.com>
2021-01-27 20:03:26 +0100

    PrimerTest

--- Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
@@ -4,11 +4,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Modelo.Modelo;
+
 public class ModeloTest {
 
+	private Modelo funciones = new Modelo();
+	private int resultado;
+	private int resultadoEsperado;
+	private int NTransaccion;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMostrarNumeroTransaccion() {
+		resultadoEsperado = 1;
+		resultado = funciones.MostrarNumeroTransaccion();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
 	}
-
+	
+	public void testAumentarNumeroTransaccion() {
+		NTransaccion = funciones.MostrarNumeroTransaccion();
+		resultadoEsperado = 2;
+		resultado = funciones.AumentarNumeroTransaccion();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	
 }

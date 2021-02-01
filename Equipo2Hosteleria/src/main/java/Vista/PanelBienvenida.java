beb950e8504e038bc5f2153e99962b5f2f5b6505
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-02-01 09:12:18 +0100

    Pantalla de iniciar sesion

--- Equipo2Hosteleria/src/main/java/Vista/PanelBienvenida.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelBienvenida.java
@@ -9,11 +9,12 @@ import Controlador.ControladorPanelBienvenida;
 import java.awt.Color;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class PanelBienvenida extends JPanel{
 
-	private JButton btnOperatividad;
+	private JButton btnIniciarSesion;
 	private JLabel lblBienvenida;
 	private ControladorPanelBienvenida controladorPanelBienvenida;
 	
@@ -28,15 +29,43 @@ public class PanelBienvenida extends JPanel{
 		lblBienvenida.setBounds(40, 35, 539, 32);
 		add(lblBienvenida);
 		
-		btnOperatividad = new JButton("Operatividad");
-		btnOperatividad.setBounds(250, 218, 120, 23);
-		add(btnOperatividad);
+		btnIniciarSesion = new JButton("Iniciar Sesion");
+		btnIniciarSesion.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnIniciarSesion.setBounds(183, 337, 126, 32);
+		add(btnIniciarSesion);
+		
+		JTextPane textPane_Usuario = new JTextPane();
+		textPane_Usuario.setEnabled(false);
+		textPane_Usuario.setBounds(215, 167, 189, 22);
+		add(textPane_Usuario);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblUsuario.setBounds(231, 133, 157, 23);
+		add(lblUsuario);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setHorizontalAlignment(SwingConstants.CENTER);
+		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblContrasea.setBounds(231, 226, 157, 23);
+		add(lblContrasea);
+		
+		JTextPane textPane_Contrasea = new JTextPane();
+		textPane_Contrasea.setEnabled(false);
+		textPane_Contrasea.setBounds(215, 260, 189, 22);
+		add(textPane_Contrasea);
+		
+		JButton btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnRegistrarse.setBounds(309, 337, 126, 32);
+		add(btnRegistrarse);
 		
 		initializeEvents();
 	}
 	
 	private void initializeEvents() {
-		this.btnOperatividad.addActionListener(listenerBotonOperatividad(this.controladorPanelBienvenida));
+		this.btnIniciarSesion.addActionListener(listenerBotonOperatividad(this.controladorPanelBienvenida));
 	}
 	
 	private ActionListener listenerBotonOperatividad(ControladorPanelBienvenida controladorPanelBienvenida) {

fa14ebccd3a2c56e6ae16693ea18f577c56ba209
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-02-03 12:23:13 +0100

    Creados el resto de tests del Modelo

--- Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
@@ -14,9 +14,11 @@ public class ModeloTest {
 	private int resultado;
 	private double resultadoDouble;
 	private String resultadoString;
+	private boolean resultadoBoolean;
 	private int resultadoEsperado;
 	private double resultadoEsperadoDouble;
 	private String resultadoEsperadoString;
+	private boolean resultadoEsperadoBoolean;
 	private int NTransaccion=1;
 	private String arrayNombresProducto[]= {"Garbanzos","Paella","Bacalao","Hamburguesa","Cerveza"};
 	
@@ -41,6 +43,27 @@ public class ModeloTest {
 		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
 	}
 	@Test
+	public void testTotalCarro() {
+		resultadoEsperadoDouble = 10;
+		funciones.setTotalCarro().añadirCoste(5,2);
+		resultadoDouble = funciones.getTotalCarro().leerCoste();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void testResetCarroString() {
+		resultadoEsperadoString = "";
+		funciones.resetCarro().borrarListaCompra();
+		resultadoString = funciones.getListaCompra().leerLista();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void testResetCarroDouble() {
+		resultadoEsperadoDouble = 0;
+		funciones.resetCarro().borrarListaCompra();
+		resultadoDouble = funciones.getTotalCarro().leerCoste();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
 	public void testMostrarNumeroTransaccion() {
 		resultadoEsperado = NTransaccion;
 		resultado = funciones.mostrarNumeroTransaccion().leerNumTrans();
@@ -50,6 +73,12 @@ public class ModeloTest {
 	public void testAumentarNumeroTransaccion() {
 		resultadoEsperado = NTransaccion++;
 		resultado = funciones.aumentarNumeroTransaccion().sumarNumTrans();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void testPruebaConexion() {
+		resultadoEsperadoBoolean = true;
+		resultadoBoolean = funciones.pruebaConexion().probarConexion();
 		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
 	}
 	

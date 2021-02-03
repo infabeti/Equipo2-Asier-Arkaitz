aea2e6f8da9fa49584c7972d8d78b9c7261a822e
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-02-03 12:14:31 +0100

    3 Tests añadidos

--- Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
@@ -5,15 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import Modelo.Modelo;
+import Modelo.Producto;
 
 @SuppressWarnings({ "static-access", "unused" })
 public class ModeloTest {
 
 	private Modelo funciones = new Modelo();
 	private int resultado;
+	private double resultadoDouble;
+	private String resultadoString;
 	private int resultadoEsperado;
+	private double resultadoEsperadoDouble;
+	private String resultadoEsperadoString;
 	private int NTransaccion=1;
+	private String arrayNombresProducto[]= {"Garbanzos","Paella","Bacalao","Hamburguesa","Cerveza"};
 	
+
+	@Test
+	public void testGetNombresProductos() {
+		String[] resultadoEsperadoArray = arrayNombresProducto;
+		String[] resultadoArray = funciones.getNombresProductos().nombresProductos();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void testGetPrecioVentaProductos() {
+		resultadoEsperadoDouble = 5;
+		resultadoDouble = funciones.getPrecioVentaProductos().precioVentaProductos("Hamburguesa");
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void testListaCompra() {
+		resultadoEsperadoString = "Hamburguesa - Cantidad: "+2+" - Precio: "+5+"€ - Total: "+10+"€\n";
+		funciones.setListaCompra().añadirProductos("Hamburguesa",5,2);
+		resultadoString = funciones.getListaCompra().leerLista();
+		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+	}
 	@Test
 	public void testMostrarNumeroTransaccion() {
 		resultadoEsperado = NTransaccion;

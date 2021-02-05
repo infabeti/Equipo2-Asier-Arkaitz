f6b417f0a7bf5aec0c57d27110a2a19a7612a3a2
DavidLopez89 <david.lopezri@elorrieta-errekamari.com>
2021-02-05 12:48:53 +0100

    actualizacion del panel Registro, creacion del modelo Usuario

---
+++ Equipo2Hosteleria/src/main/java/Modelo/Usuario.java
@@ -0,0 +1,99 @@
+package Modelo;
+
+import javax.swing.ComboBoxModel;
+
+public class Usuario {
+
+	protected String usuario;
+	protected String negocio;
+	protected String contraseña;
+	protected String nombre;
+	protected String apellidos;
+	protected String nombreempresa;
+	protected String nif;
+	
+			
+	//Constructor con parametros
+	public Usuario(String Usuario, String Contraseña, String Negocio, String Nombre, String Apellidos, String NombreEmpresa, String NIF) {
+		this.usuario = Usuario;
+		this.negocio = Negocio;
+		this.contraseña = Contraseña;
+		this.nombre = Nombre;
+		this.apellidos = Apellidos;
+		this.nombreempresa = NombreEmpresa;
+		this.nif = NIF;
+
+			}
+	
+	//get y set
+	//get
+	public String getUsuario() {
+				
+		return this.usuario;
+	}
+	
+	public String getTipo() {
+		
+		return this.negocio;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+		
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public String getNombreempresa() {
+		return nombreempresa;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public String getContraseña() {
+		
+		return this.contraseña;
+	}
+	
+	
+	
+			
+	//set
+	public void setUsuario(String Usuario)
+	{
+		this.usuario = Usuario;
+	}
+	
+	public void setTipo(String Negocio)
+	{
+		this.negocio = Negocio;
+	}
+	
+	public void setContraseña(String Contraseña)
+	{
+		this.contraseña = Contraseña;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	public void setNombreempresa(String nombreempresa) {
+		this.nombreempresa = nombreempresa;
+	}
+	
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	
+	
+}
+
--- Equipo2Hosteleria/src/main/java/Vista/PanelRegistro.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelRegistro.java
@@ -33,6 +33,9 @@ public class PanelRegistro extends JPanel{
 	private JButton btnVolver;
 
 	private ControladorPanelRegistro controladorPanelRegistro;
+	private JTextField textNombre;
+	private JTextField textApellidos;
+	private JTextField textField;
 	
 	
 	
@@ -41,70 +44,93 @@ public class PanelRegistro extends JPanel{
 		
 		setBackground(new Color(102, 153, 255));
 		this.controladorPanelRegistro = controladorPanelRegistro;
-		
 		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Registro de Usuario");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 11, 600, 28);
+		lblNewLabel.setBounds(95, 11, 450, 28);
 		add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Introduce un nombre de usuario");
+		JLabel lblNewLabel_1 = new JLabel("Introduce  tu DNI");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(85, 83, 450, 14);
+		lblNewLabel_1.setBounds(54, 50, 189, 14);
 		add(lblNewLabel_1);
 		
 		textUsuario = new JTextField();
-		textUsuario.setBounds(230, 108, 159, 20);
+		textUsuario.setBounds(79, 75, 102, 20);
 		add(textUsuario);
 		textUsuario.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Introduce tu contrase\u00F1a");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(184, 160, 251, 14);
+		lblNewLabel_2.setBounds(328, 50, 206, 14);
 		add(lblNewLabel_2);
 		
 		textContraseña1 = new JTextField();
-		textContraseña1.setBounds(230, 185, 159, 20);
+		textContraseña1.setBounds(370, 75, 153, 20);
 		add(textContraseña1);
 		textContraseña1.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Repetir contrase\u00F1a");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setBounds(230, 226, 159, 14);
+		lblNewLabel_3.setBounds(370, 130, 159, 14);
 		add(lblNewLabel_3);
 		
 		textContraseña2 = new JTextField();
-		textContraseña2.setBounds(230, 251, 159, 20);
+		textContraseña2.setBounds(370, 155, 153, 20);
 		add(textContraseña2);
 		textContraseña2.setColumns(10);
 		
 		btnCrear = new JButton("Crear");
-		btnCrear.setBounds(54, 354, 89, 23);
+		btnCrear.setBounds(79, 375, 126, 32);
 		add(btnCrear);
 		
 		btnVolver = new JButton("Volver");	
-		btnVolver.setBounds(487, 354, 89, 23);
+		btnVolver.setBounds(409, 375, 126, 32);
 		add(btnVolver);
 		
+		JLabel lblNewLabel_5 = new JLabel("Introduce tu nombre");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_5.setBounds(79, 130, 198, 14);
+		add(lblNewLabel_5);
+		
+		textNombre = new JTextField();
+		textNombre.setColumns(10);
+		textNombre.setBounds(79, 155, 132, 20);
+		add(textNombre);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("Introduce tu apellido");
+		lblNewLabel_5_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_5_1.setBounds(76, 213, 201, 14);
+		add(lblNewLabel_5_1);
+		
+		textApellidos = new JTextField();
+		textApellidos.setColumns(10);
+		textApellidos.setBounds(79, 238, 132, 20);
+		add(textApellidos);
+		
+		JLabel lblNewLabel_6 = new JLabel("Introduce el Nombre del local");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_6.setBounds(287, 213, 250, 14);
+		add(lblNewLabel_6);
+		
+		textField = new JTextField();
+		textField.setBounds(397, 238, 126, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Selecciona un tipo de local");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_4.setBounds(76, 290, 225, 14);
+		add(lblNewLabel_4);
+		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Bar", "Cafeteria"}));
-		comboBox.setBounds(262, 321, 96, 22);
+		comboBox.setBounds(79, 317, 148, 22);
 		add(comboBox);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("Selecciona tu tipo de negocio");
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel_2_1.setBounds(184, 296, 251, 14);
-		add(lblNewLabel_2_1);
-		
-	
-		
-		
 		
 		
 		initializeEvents();

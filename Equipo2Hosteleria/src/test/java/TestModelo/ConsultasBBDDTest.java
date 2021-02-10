b2faf9cd9d2783325cc76a188af791d04930c901
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-02-10 09:31:33 +0100

    Progreso Tests 2

--- Equipo2Hosteleria/src/test/java/TestModelo/ConsultasBBDDTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ConsultasBBDDTest.java
@@ -18,29 +18,31 @@ public class ConsultasBBDDTest {
 	private double resultadoEsperadoDouble;
 	private String resultadoEsperadoString;
 	private boolean resultadoEsperadoBoolean;
+	private String[] resultadoArrayString;
+	private String[] resultadoEsperadoArrayString;
+	private Producto[] arrayProductos;
+	private Producto[] arrayProductosEsperado;
 	private int NTransaccion=1;
 	private String arrayNombresProducto[]= {"Garbanzos","Paella","Bacalao","Hamburguesa","Cerveza"};
-	private Producto arrayProducto[];
-	
 	@Test
 	public void testGetListaProductos() {
-		arrayProducto = new Producto[5];
-		arrayProducto[0] = new Producto("Garbanzos","Comida","30-01-2021",5,5);
-		arrayProducto[1] = new Producto("Paella","Comida","31-01-2021",5,5);
-		arrayProducto[2] = new Producto("Bacalao","Comida","02-02-2021",5,5);
-		arrayProducto[3] = new Producto("Hamburguesa","Comida","30-01-2021",5,5);
-		arrayProducto[4] = new Producto("Cerveza","Bebida","01-02-2021",5,5);
-		Producto[] resultadoEsperadoArray = arrayProducto;
-		Producto[] resultadoArray = consultasBBDD.getListaProductos();
+		arrayProductosEsperado = new Producto[5];
+		arrayProductosEsperado[0] = new Producto("Garbanzos","Comida","30-01-2021",5,5);
+		arrayProductosEsperado[1] = new Producto("Paella","Comida","31-01-2021",5,5);
+		arrayProductosEsperado[2] = new Producto("Bacalao","Comida","02-02-2021",5,5);
+		arrayProductosEsperado[3] = new Producto("Hamburguesa","Comida","30-01-2021",5,5);
+		arrayProductosEsperado[4] = new Producto("Cerveza","Bebida","01-02-2021",5,5);
+		arrayProductos = consultasBBDD.getListaProductos();
+		resultadoEsperadoArrayString = arrayNombresProducto;
+		resultadoArrayString = consultasBBDD.nombresProductos();
 		//org.junit.Assert.assertEquals(resultadoEsperadoArray, resultadoArray);
-		assertArrayEquals(resultadoEsperadoArray, resultadoArray);
-	}
-	/*
+		assertArrayEquals(resultadoEsperadoArrayString, resultadoArrayString);
+	}/*
 	@Test
 	public void testNombresProductos() {
-		String[] resultadoEsperadoArray = arrayNombresProducto;
-		String[] resultadoArray = consultasBBDD.getListaProductos();
-		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
+		resultadoEsperadoArrayString = arrayNombresProducto;
+		resultadoArrayString = consultasBBDD.nombresProductos();
+		assertArrayEquals(resultadoEsperadoArrayString, resultadoArrayString);
 	}
 	@Test
 	public void testGetPrecioVentaProductos() {

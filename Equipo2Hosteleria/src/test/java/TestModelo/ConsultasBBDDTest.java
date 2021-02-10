0321d4855875b0d4d5ffa03200c4e23c7099d9da
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-02-10 09:08:23 +0100

    Progreso Tests 1

--- Equipo2Hosteleria/src/test/java/TestModelo/ConsultasBBDDTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ConsultasBBDDTest.java
@@ -4,17 +4,12 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import org.junit.Test;
 
-import Modelo.CarroCompra;
 import Modelo.ConsultasBBDD;
-import Modelo.Modelo;
 import Modelo.Producto;
 
-@SuppressWarnings({ "unused" })
 public class ConsultasBBDDTest {
 
-	private static ConsultasBBDD consultasBBDD;
-	private static CarroCompra carroCompra;
-	private Modelo funciones = new Modelo(consultasBBDD, carroCompra);
+	private ConsultasBBDD consultasBBDD = new ConsultasBBDD();
 	private int resultado;
 	private double resultadoDouble;
 	private String resultadoString;
@@ -25,12 +20,26 @@ public class ConsultasBBDDTest {
 	private boolean resultadoEsperadoBoolean;
 	private int NTransaccion=1;
 	private String arrayNombresProducto[]= {"Garbanzos","Paella","Bacalao","Hamburguesa","Cerveza"};
+	private Producto arrayProducto[];
 	
-
 	@Test
-	public void testGetNombresProductos() {
+	public void testGetListaProductos() {
+		arrayProducto = new Producto[5];
+		arrayProducto[0] = new Producto("Garbanzos","Comida","30-01-2021",5,5);
+		arrayProducto[1] = new Producto("Paella","Comida","31-01-2021",5,5);
+		arrayProducto[2] = new Producto("Bacalao","Comida","02-02-2021",5,5);
+		arrayProducto[3] = new Producto("Hamburguesa","Comida","30-01-2021",5,5);
+		arrayProducto[4] = new Producto("Cerveza","Bebida","01-02-2021",5,5);
+		Producto[] resultadoEsperadoArray = arrayProducto;
+		Producto[] resultadoArray = consultasBBDD.getListaProductos();
+		//org.junit.Assert.assertEquals(resultadoEsperadoArray, resultadoArray);
+		assertArrayEquals(resultadoEsperadoArray, resultadoArray);
+	}
+	/*
+	@Test
+	public void testNombresProductos() {
 		String[] resultadoEsperadoArray = arrayNombresProducto;
-		String[] resultadoArray = funciones.getConsultasBBDD().nombresProductos();
+		String[] resultadoArray = consultasBBDD.getListaProductos();
 		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
 	}
 	@Test
@@ -86,5 +95,5 @@ public class ConsultasBBDDTest {
 		resultadoBoolean = funciones.getConsultasBBDD().probarConexion();
 		org.junit.Assert.assertEquals(resultadoEsperado, resultado);
 	}
-	
+	*/
 }
--- Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ModeloTest.java
@@ -4,24 +4,39 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import org.junit.Test;
 
+import Modelo.Modelo;
+import Modelo.ConsultasBBDD;
+import Modelo.CarroCompra;
+
 public class ModeloTest {
 
 	private Modelo modelo;
 	private ConsultasBBDD consultasBBDDMock = mock(ConsultasBBDD.class);
 	private ConsultasBBDD consultasBBDDMock2 = mock(ConsultasBBDD.class);
+	private CarroCompra carroCompraMock = mock(CarroCompra.class);
+	private CarroCompra carroCompraMock2 = mock(CarroCompra.class);
 	
 	@Test
 	public void constructorModeloTest() {
-		modelo = new Modelo(consultasBBDDMock);
+		modelo = new Modelo(consultasBBDDMock, carroCompraMock);
 		
 		assertEquals(modelo.getConsultasBBDD(), consultasBBDDMock);
+		assertEquals(modelo.getCarroCompra(), carroCompraMock);
 	}
 
 	@Test
 	public void setConsultasBBDDTest() {
-		modelo = new Modelo(consultasBBDDMock);
+		modelo = new Modelo(consultasBBDDMock, null);
 		modelo.setConsultasBBDD(consultasBBDDMock2);
 		
 		assertEquals(modelo.getConsultasBBDD(), consultasBBDDMock2);
+	}
+
+	@Test
+	public void setCarroCompraTest() {
+		modelo = new Modelo(null, carroCompraMock);
+		modelo.setCarroCompra(carroCompraMock2);
+		
+		assertEquals(modelo.getCarroCompra(), carroCompraMock2);
 	}
 }

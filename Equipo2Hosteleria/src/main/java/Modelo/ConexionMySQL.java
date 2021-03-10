adb7fe7964990d6414d6b42979e85af049161f21
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-03-10 10:55:58 +0100

    Pedidos ya registra todo correctamente

--- Equipo2Hosteleria/src/main/java/Modelo/ConexionMySQL.java
+++ Equipo2Hosteleria/src/main/java/Modelo/ConexionMySQL.java
@@ -108,26 +108,28 @@ public class ConexionMySQL {
 				 
 		try {
 			Conexion();
-			ps = con.prepareStatement("INSERT INTO ticket(num_trans, fecha, nif_local) VAlUES(?,?,?)");
+			ps = con.prepareStatement("INSERT INTO ticket(num_trans, fecha, nif_local, tipo) VAlUES(?,?,?,?)");
 			 
 			ps.setInt(1,tic.getNTransaccion());
 			ps.setString(2,tic.getFecha());
 			ps.setString(3,tic.getNif_local());
+			ps.setString(4,tic.getTipo());
 			 
 			ps.executeUpdate();
 	
-			String sql = "select*from ticket where num_trans = ? and fecha =  ? and nif_local =  ?";
+			String sql = "select*from ticket where num_trans = ? and fecha =  ? and nif_local =  ? and tipo =  ?";
 				
 			ps = con.prepareStatement(sql); 
 			 
 			ps.setInt(1,tic.getNTransaccion());
 			ps.setString(2,tic.getFecha());
 			ps.setString(3,tic.getNif_local());
-		
+			ps.setString(4,tic.getTipo());
+			
 			rs = ps.executeQuery();
 		 
 			while (rs.next()) {
-				ticket1 = new Ticket(rs.getInt(1), rs.getString(2), rs.getString(3), sql);			
+				ticket1 = new Ticket(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));			
 			}
 		 
 			System.out.println("Se ha creado el ticket correctamente");
@@ -174,14 +176,14 @@ public class ConexionMySQL {
 	 
 	public Pedido registrarPedidoConDomicilio(Pedido pedi) {
 		 
-		Pedido Pedido1=null;
+		Pedido pedido1=null;
 				 
 		try {
 			Conexion();
 			ps = con.prepareStatement("INSERT INTO pedido(num_trans, tipo, domicilio) VAlUES(?,?,?)");
 				 
 			ps.setInt(1,pedi.getNTransaccion());
-			ps.setString(2,pedi.getTipo());
+			ps.setString(2,pedi.getTipoPedido());
 			ps.setString(3,pedi.getDomicilio());
 			
 			ps.executeUpdate();
@@ -191,32 +193,32 @@ public class ConexionMySQL {
 			pst = con.prepareStatement(sql); 
 				 
 			pst.setInt(1,pedi.getNTransaccion());
-			pst.setString(2,pedi.getTipo());
+			pst.setString(2,pedi.getTipoPedido());
 			pst.setString(3,pedi.getDomicilio());
 
 			rs = pst.executeQuery();
 			 
 			while (rs.next()) {
-				Pedido1 = new Pedido(rs.getInt(1), rs.getString(2), rs.getString(3));			
+				pedido1 = new Pedido(rs.getInt(1), rs.getString(2), rs.getString(3));			
 			}
 			System.out.println("Se ha creado el pedido correctamente");
 			
 		} catch (Exception e) {
 			System.out.println("Error en creacion del Pedido");
 		}
-		return Pedido1;
+		return pedido1;
 	}
 
 	public Pedido registrarPedidoSinDomicilio(Pedido pedi) {
 		
-		Pedido Pedido1=null;
+		Pedido pedido1=null;
 					 
 		try {
 			Conexion();
 			ps = con.prepareStatement("INSERT INTO pedido(num_trans, tipo) VAlUES(?,?)");
 				 
 			ps.setInt(1,pedi.getNTransaccion());
-			ps.setString(2,pedi.getTipo());
+			ps.setString(2,pedi.getTipoPedido());
 		
 			ps.executeUpdate();
 
@@ -225,12 +227,12 @@ public class ConexionMySQL {
 			pst = con.prepareStatement(sql); 
 				 
 			pst.setInt(1,pedi.getNTransaccion());
-			pst.setString(2,pedi.getTipo());
+			pst.setString(2,pedi.getTipoPedido());
 			
 			rs = pst.executeQuery();
 			 
 			while (rs.next()) {
-				Pedido1 = new Pedido(rs.getInt(1), rs.getString(2));			
+				pedido1 = new Pedido(rs.getInt(1), rs.getString(2));			
 			}
 			
 			System.out.println("Se ha creado el pedido correctamente");
@@ -238,7 +240,7 @@ public class ConexionMySQL {
 		}catch (Exception e) {
 			System.out.println("Error en creacion del Pedido");
 		}
-		return Pedido1;
+		return pedido1;
 	}
 	
 	public Identificacion registrarIdentificacion(Identificacion ide) {
@@ -279,11 +281,11 @@ public class ConexionMySQL {
 	}
 	 
 	public Incluye registrarIncluye(Incluye inc) {
-		 Incluye Incluye1=null;
+		 Incluye incluye1=null;
 
 		 try {
 			 Conexion();
-			 ps = con.prepareStatement("INSERT INTO Incluye(nombre_producto, num_trans, cantidad, precio) VAlUES(?,?,?,?,?)");
+			 ps = con.prepareStatement("INSERT INTO Incluye(nombre_producto, num_trans, cantidad, precio) VAlUES(?,?,?,?)");
 	
 			 ps.setString(1,inc.getNombreProducto());
 			 ps.setInt(2,inc.getNtransaccion());
@@ -292,7 +294,7 @@ public class ConexionMySQL {
 		
 			 ps.executeUpdate();
 	
-			 String sql = "select*from Incluye where nombre_producto = ? and num_trans = ? and cantidad = ? and precio_venta = ? and precio_compra = ?";
+			 String sql = "select*from Incluye where nombre_producto = ? and num_trans = ? and cantidad = ? and precio = ?";
 	
 			 pst = con.prepareStatement(sql);
 	
@@ -304,7 +306,7 @@ public class ConexionMySQL {
 			 rs = pst.executeQuery();
 	
 			 while (rs.next()) {
-				 Incluye1 = new Incluye(rs.getString(1), rs.getInt(2),rs.getInt(3),rs.getDouble(4));
+				 incluye1 = new Incluye(rs.getString(1), rs.getInt(2),rs.getInt(3),rs.getDouble(4));
 			 }
 			 
 			 System.out.println("Se ha creado la Incluye correctamente");
@@ -312,10 +314,10 @@ public class ConexionMySQL {
 		 } catch (Exception e) {
 			 System.out.println("Error en creacion de la Incluye");
 		 }
-		 return Incluye1;
+		 return incluye1;
 	}
 	
-	public static String nifLocal(String nombreLocal) {
+	public String nifLocal(String nombreLocal) {
 		Local local=null;
 		
 	try {
@@ -340,7 +342,7 @@ public class ConexionMySQL {
 	}	
  }
 	
-	public static String[] nombreLocal() {
+	public String[] nombreLocal() {
 		Local local=null;
 		String [] registro = new String[3];
 		int i=0;

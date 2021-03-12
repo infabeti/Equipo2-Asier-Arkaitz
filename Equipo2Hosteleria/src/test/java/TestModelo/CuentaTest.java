89c5dce79394d243a29693fe1d878971f5dc5ba1
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-03-12 12:05:28 +0100

    Test Cuenta

--- Equipo2Hosteleria/src/test/java/TestModelo/CuentaTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/CuentaTest.java
@@ -15,42 +15,15 @@ public class CuentaTest {
 	
 	@Test
 	public void testCrearUsuario() {
-		//crearUsuario(String dni, String contrasena, String nombre, String apellidos, String nif_local);
-		cuenta.crearUsuario("12345678A", "123", "John", "Smith", "87654321Z");
-		resultadoEsperadoString = "Hamburguesa";
-		carroCompra.anadirProducto("Hamburguesa",5,2);
-		Object[][] resultadoObjeto = carroCompra.getCarroCompra();
-		org.junit.Assert.assertEquals(resultadoEsperadoString, resultadoObjeto[0][0]);
+		boolean resultadoBoolean = cuenta.crearUsuario("12345678A", "123", "John", "Smith", "87654321Z");
+		org.junit.Assert.assertEquals(false, resultadoBoolean);
 	}
+	
 	@Test
-	public void testListaCompraRepe() {
-		resultadoEsperadoString = "Hamburguesa";
-		carroCompra.anadirProducto("Hamburguesa",5,2);
-		Object[][] resultadoObjeto = carroCompra.getCarroCompra();
-		carroCompra.anadirProducto("Hamburguesa",5,2);
-		org.junit.Assert.assertEquals(resultadoEsperadoString, resultadoObjeto[0][0]);
-	}
-
-	@Test
-	public void testListaCompraAumento() {
-		resultadoEsperadoString = "Hamburguesa";
-		carroCompra.anadirProducto("Hamburguesa",5,2);
-		Object[][] resultadoObjeto = carroCompra.getCarroCompra();
-		carroCompra.anadirProducto("Cerveza",5,1);
-		org.junit.Assert.assertEquals(resultadoEsperadoString, resultadoObjeto[0][0]);
-	}
-	@Test
-	public void testTotalCarro() {
-		resultadoEsperadoDouble = 10;
-		carroCompra.anadirProducto("Hamburguesa",5,2);
-		resultadoDouble = carroCompra.getTotalCarro();
-		org.junit.Assert.assertEquals(resultadoEsperadoDouble, resultadoDouble, 1);
-	}
-	@Test
-	public void testResetCarroDouble() {
-		resultadoEsperadoDouble = 0;
-		carroCompra.borrarCarroCompra();
-		resultadoDouble = carroCompra.getTotalCarro();
-		org.junit.Assert.assertEquals(resultadoEsperadoDouble, resultadoDouble, 1);
+	public void testIniciarUsuario() {
+		boolean resultadoBoolean = cuenta.iniciarUsuario("11111111A", "111");
+		org.junit.Assert.assertEquals(true, resultadoBoolean);
+		resultadoBoolean = cuenta.iniciarUsuario("11111111B", "112");
+		org.junit.Assert.assertEquals(false, resultadoBoolean);
 	}
 }

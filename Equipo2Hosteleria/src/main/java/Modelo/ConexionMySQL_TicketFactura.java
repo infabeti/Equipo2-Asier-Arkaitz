040ce61a8f3f10f045162e1ed1af8b2e59427f06
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-03-15 09:22:26 +0100

    Conexion TicketFactura Hecho

--- Equipo2Hosteleria/src/main/java/Modelo/ConexionMySQL_TicketFactura.java
+++ Equipo2Hosteleria/src/main/java/Modelo/ConexionMySQL_TicketFactura.java
@@ -9,14 +9,17 @@ public class ConexionMySQL_TicketFactura {
 	private static Connection con=null;
 	private static PreparedStatement ps = null;
 	private static ResultSet rs=null;
+	private static final String SETTICKET = "INSERT INTO ticket(num_trans, fecha, nif_local, tipo) VAlUES(?,?,?,?)";
+	private static final String GETTICKET = "select*from ticket where num_trans = ? and fecha =  ? and nif_local =  ? and tipo =  ?";
+	private static final String SETFACTURA = "INSERT INTO factura(num_trans, nif) VAlUES(?,?)";
+	private static final String GETFACTURA = "select*from factura where num_trans = ? and nif = ?";
 	
 	public Ticket registrarTicket(Ticket tic) {
-		
 		Ticket ticket1=null;
 				 
 		try {
 			ConexionMySQL.Conexion();
-			ps = con.prepareStatement("INSERT INTO ticket(num_trans, fecha, nif_local, tipo) VAlUES(?,?,?,?)");
+			ps = con.prepareStatement(SETTICKET);
 			 
 			ps.setInt(1,tic.getNTransaccion());
 			ps.setString(2,tic.getFecha());
@@ -25,9 +28,7 @@ public class ConexionMySQL_TicketFactura {
 			 
 			ps.executeUpdate();
 	
-			String sql = "select*from ticket where num_trans = ? and fecha =  ? and nif_local =  ? and tipo =  ?";
-				
-			ps = con.prepareStatement(sql); 
+			ps = con.prepareStatement(GETTICKET); 
 			 
 			ps.setInt(1,tic.getNTransaccion());
 			ps.setString(2,tic.getFecha());
@@ -39,7 +40,7 @@ public class ConexionMySQL_TicketFactura {
 			while (rs.next()) {
 				ticket1 = new Ticket(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));			
 			}
-		 
+			
 			System.out.println("Se ha creado el ticket correctamente");
 		 
 		} catch (Exception e) {
@@ -54,16 +55,14 @@ public class ConexionMySQL_TicketFactura {
 					 
 		try {
 			ConexionMySQL.Conexion();
-			ps = con.prepareStatement("INSERT INTO factura(num_trans, nif) VAlUES(?,?)");
+			ps = con.prepareStatement(SETFACTURA);
 				 
 			ps.setInt(1,fac.getNTransaccion());
 			ps.setString(2,fac.getNif());
 			
 			ps.executeUpdate();
 
-			String sql = "select*from factura where num_trans = ? and nif = ?";
-					
-			ps = con.prepareStatement(sql); 
+			ps = con.prepareStatement(GETFACTURA); 
 				 
 			ps.setInt(1,fac.getNTransaccion());
 			ps.setString(2,fac.getNif());

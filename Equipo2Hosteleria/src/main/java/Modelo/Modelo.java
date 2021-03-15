1e18071400ce73f3087b2747bd3dc0d6d50d9139
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-03-15 10:07:18 +0100

    Conexiones nuevas WIP2 preparacion Modelo

--- Equipo2Hosteleria/src/main/java/Modelo/Modelo.java
+++ Equipo2Hosteleria/src/main/java/Modelo/Modelo.java
@@ -4,8 +4,11 @@ public class Modelo {
 
 	private ConsultasBBDD consultasBBDD;
 	private ConexionMySQL conexionMySQL;
-	private ConexionMySQL_Usuarios conexionMySQL_Usuarios;
+	private ConexionMySQL_IdentIncluye conexionMySQL_IdentIncluye;
 	private ConexionMySQL_Local conexionMySQL_Local;
+	private ConexionMySQL_Pedido conexionMySQL_Pedido;
+	private ConexionMySQL_TicketFactura conexionMySQL_TicketFactura;
+	private ConexionMySQL_Usuarios conexionMySQL_Usuarios;
 	private CarroCompra carroCompra;
 	private Cuenta cuenta;
 	private Pago pago;
@@ -14,8 +17,11 @@ public class Modelo {
 	public Modelo() {
 		consultasBBDD = new ConsultasBBDD();
 		conexionMySQL = new ConexionMySQL();
-		conexionMySQL_Usuarios = new ConexionMySQL_Usuarios();
+		conexionMySQL_IdentIncluye = new ConexionMySQL_IdentIncluye();
 		conexionMySQL_Local = new ConexionMySQL_Local();
+		conexionMySQL_Pedido = new ConexionMySQL_Pedido();
+		conexionMySQL_TicketFactura = new ConexionMySQL_TicketFactura();
+		conexionMySQL_Usuarios = new ConexionMySQL_Usuarios();
 		carroCompra = new CarroCompra();
 		cuenta = new Cuenta();
 		pago = new Pago();
@@ -30,57 +36,61 @@ public class Modelo {
 		this.consultasBBDD = consultasBBDD;
 	}
 
-	public ConexionMySQL getConexionMySQL() {
-		return conexionMySQL;
-	}
+	public ConexionMySQL getConexionMySQL() { return conexionMySQL;	}
 
 	public void setConexionMySQL(ConexionMySQL conexionMySQL) {
 		this.conexionMySQL = conexionMySQL;
 	}
 
-	public ConexionMySQL_Usuarios getConexionMySQL_Usuarios() {
-		return conexionMySQL_Usuarios;
+	public ConexionMySQL_IdentIncluye getConexionMySQL_IdentIncluye() {	return conexionMySQL_IdentIncluye; }
+
+	public void setConexionMySQL_IdentIncluye(ConexionMySQL_IdentIncluye conexionMySQL_IdentIncluye) {
+		this.conexionMySQL_IdentIncluye = conexionMySQL_IdentIncluye;
 	}
 
-	public void setConexionMySQL_Usuarios(ConexionMySQL_Usuarios conexionMySQL_Usuarios) {
-		this.conexionMySQL_Usuarios = conexionMySQL_Usuarios;
-	}
-
-	public ConexionMySQL_Local getConexionMySQL_Local() {
-		return conexionMySQL_Local;
-	}
+	public ConexionMySQL_Local getConexionMySQL_Local() { return conexionMySQL_Local; }
 
 	public void setConexionMySQL_Local(ConexionMySQL_Local conexionMySQL_Local) {
 		this.conexionMySQL_Local = conexionMySQL_Local;
 	}
-	
-	public CarroCompra getCarroCompra() {
-		return this.carroCompra;
+
+	public ConexionMySQL_Pedido getConexionMySQL_Pedido() {	return conexionMySQL_Pedido; }
+
+	public void setConexionMySQL_Pedido(ConexionMySQL_Pedido conexionMySQL_Pedido) {
+		this.conexionMySQL_Pedido = conexionMySQL_Pedido;
 	}
+
+	public ConexionMySQL_TicketFactura getConexionMySQL_TicketFactura() { return conexionMySQL_TicketFactura; }
+
+	public void setConexionMySQL_TicketFactura(ConexionMySQL_TicketFactura conexionMySQL_TicketFactura) {
+		this.conexionMySQL_TicketFactura = conexionMySQL_TicketFactura;
+	}
+
+	public ConexionMySQL_Usuarios getConexionMySQL_Usuarios() {	return conexionMySQL_Usuarios;	}
+
+	public void setConexionMySQL_Usuarios(ConexionMySQL_Usuarios conexionMySQL_Usuarios) {
+		this.conexionMySQL_Usuarios = conexionMySQL_Usuarios;
+	}
+	
+	public CarroCompra getCarroCompra() { return this.carroCompra;	}
 	
 	public void setCarroCompra(CarroCompra carroCompra) {
 		this.carroCompra = carroCompra;
 	}
 
-	public Cuenta getCuenta() {
-		return cuenta;
-	}
+	public Cuenta getCuenta() {	return cuenta;	}
 
 	public void setCuenta(Cuenta cuenta) {
 		this.cuenta = cuenta;
 	}
 
-	public Pago getPago() {
-		return pago;
-	}
+	public Pago getPago() {	return pago; }
 
 	public void setPago(Pago pago) {
 		this.pago = pago;
 	}
 
-	public Local getLocal() {
-		return local;
-	}
+	public Local getLocal() { return local; }
 
 	public void setLocal(String dni) {
 		this.local = getConexionMySQL_Local().obtenerLocal(dni);

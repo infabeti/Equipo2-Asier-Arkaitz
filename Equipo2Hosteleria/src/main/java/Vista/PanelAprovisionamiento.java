ff43c3c88dfb6d6795250dcdc9f96ed0b2d798e3
DavidLopez89 <david.lopezri@elorrieta-errekamari.com>
2021-03-22 10:27:29 +0100

    creacion pantalla aprovisionamiento

--- Equipo2Hosteleria/src/main/java/Vista/PanelAprovisionamiento.java
+++ Equipo2Hosteleria/src/main/java/Vista/PanelAprovisionamiento.java
@@ -10,6 +10,12 @@ import javax.swing.SwingConstants;
 
 import Controlador.ControladorPanelAprovisionamiento;
 import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.DefaultComboBoxModel;
 
 @SuppressWarnings("serial")
 public class PanelAprovisionamiento extends JPanel{
@@ -17,6 +23,11 @@ public class PanelAprovisionamiento extends JPanel{
 	private JButton btnVolver;
 	private JButton btnDesconectarse;
 	private ControladorPanelAprovisionamiento controladorPanelAprovisionamiento;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTable table;
+	private JTextField textField_6;
 	
 	public PanelAprovisionamiento(ControladorPanelAprovisionamiento controladorPanelAprovisionamiento) {
 		setBackground(new Color(102, 153, 255));
@@ -30,12 +41,94 @@ public class PanelAprovisionamiento extends JPanel{
 		add(lblAprovisionamiento);
 		
 		btnDesconectarse = new JButton("Desconectarse");
-		btnDesconectarse.setBounds(137, 235, 146, 23);
+		btnDesconectarse.setBounds(139, 366, 146, 23);
 		add(btnDesconectarse);
 		
 		btnVolver = new JButton("Volver");
-		btnVolver.setBounds(38, 235, 89, 23);
+		btnVolver.setBounds(40, 366, 89, 23);
 		add(btnVolver);
+		
+		textField = new JTextField();
+		textField.setBounds(74, 76, 86, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("N\u00BA Pedido:");
+		lblNewLabel.setBounds(20, 78, 51, 14);
+		add(lblNewLabel);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(74, 107, 86, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Fecha:");
+		lblNewLabel_1.setBounds(38, 110, 33, 14);
+		add(lblNewLabel_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(74, 138, 86, 20);
+		add(textField_2);
+		
+		JLabel lblLocal = new JLabel("Local:");
+		lblLocal.setBounds(40, 138, 28, 20);
+		add(lblLocal);
+		
+		JButton btnPagar = new JButton("Pagar");
+		btnPagar.setBounds(336, 366, 89, 23);
+		add(btnPagar);
+		
+		JLabel lblProductos_1 = new JLabel("Productos:");
+		lblProductos_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblProductos_1.setBounds(196, 58, 170, 22);
+		add(lblProductos_1);
+		
+		JComboBox comboBox_Productos = new JComboBox();
+		comboBox_Productos.setBounds(196, 89, 173, 22);
+		add(comboBox_Productos);
+		
+		JLabel lblCantidad = new JLabel("Cantidad:");
+		lblCantidad.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblCantidad.setBounds(196, 119, 102, 22);
+		add(lblCantidad);
+		
+		JComboBox comboBox_Cantidad = new JComboBox();
+		comboBox_Cantidad.setBounds(479, 106, 60, 22);
+		add(comboBox_Cantidad);
+		
+		JButton btnAadirAlCarro = new JButton("A\u00F1adir al carro");
+		btnAadirAlCarro.setBounds(412, 137, 172, 23);
+		add(btnAadirAlCarro);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(20, 169, 574, 147);
+		add(scrollPane);
+		
+		table = new JTable();
+		table.setToolTipText("Producto\r\n");
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Producto", "Cantidad", "Precio", "Total"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JLabel lblTotal = new JLabel("Total:");
+		lblTotal.setBounds(494, 328, 45, 19);
+		add(lblTotal);
+		
+		textField_6 = new JTextField();
+		textField_6.setText("0.0");
+		textField_6.setEditable(false);
+		textField_6.setBounds(531, 327, 63, 20);
+		add(textField_6);
+		
+		JButton btnBorrarLista = new JButton("Borrar lista");
+		btnBorrarLista.setBounds(435, 366, 146, 23);
+		add(btnBorrarLista);
 		
 
 		
@@ -64,5 +157,4 @@ public class PanelAprovisionamiento extends JPanel{
 			}
 		};
 	}
-	
 }

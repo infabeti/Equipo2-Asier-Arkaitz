08f6f82e5043e3d5743cbb8a6fc8157cf37cfd52
ALasherasSus <arkaitz.lasherassu@elorrieta-errekamari.com>
2021-03-22 08:44:53 +0100

    Arreglo test Producto y Doc BBDD

--- Equipo2Hosteleria/src/test/java/TestModelo/ProductoTest.java
+++ Equipo2Hosteleria/src/test/java/TestModelo/ProductoTest.java
@@ -8,7 +8,7 @@ import Modelo.Producto;
 
 public class ProductoTest {
 
-	private Producto p1 = new Producto("Garbanzos","Comida","30-01-2021",5,5);
+	private Producto p1 = new Producto("Garbanzos","Comida","30-01-2021",5,5,"GLUTEN");
 	
 	@Test
 	public void testCreateProducto() {
@@ -17,6 +17,7 @@ public class ProductoTest {
 		assertEquals("30-01-2021", p1.getFechaCaducidad());
 		assertEquals(5, p1.getPrecioCompra(), 0);
 		assertEquals(5, p1.getPrecioVenta(), 0);
+		assertEquals("GLUTEN", p1.getAlergeno());
 	}
 	
 	@Test
@@ -47,5 +48,11 @@ public class ProductoTest {
 	public void testPrecioVenta() {
 		p1.setPrecioVenta(7);
 		assertEquals(7, p1.getPrecioVenta(), 0);
+	}
+	
+	@Test
+	public void testAlergeno() {
+		p1.setAlergeno("MARISCO");
+		assertEquals("MARISCO", p1.getAlergeno());
 	}
 }
